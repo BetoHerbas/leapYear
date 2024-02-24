453493b4cb1c6552ba96b0036d473bec488e76c6
BetoHerbas <betitoherbas@gmail.com>
2024-02-24 18:33:25 -0400

    Should return true if year is divisible by 4

--- src/leapYear.test.js
+++ src/leapYear.test.js
@@ -4,8 +4,11 @@ describe('leapYear', () => {
   it('should return true if year is 4', () => {
     expect(leapYear(4)).toBe(true);
   });
-  it('shouldn´t return true if year is 5', () => {
+  it('should return false if year is 5', () => {
     expect(leapYear(5)).toBe(false);
+  });
+  it("should return true if year is divisible by 4", () => {
+    expect(leapYear(8)).toBe(true);
   });
 
 });
